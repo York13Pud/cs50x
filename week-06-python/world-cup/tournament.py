be1d1fcb20ea6c066dc3d9a6984c3e75ec87aa5b
dev_neil_a <neilallwood@icloud.com>
2023-12-09 21:58:13 +0000

    Completed lab 6

--- week-06-python/world-cup/tournament.py
+++ week-06-python/world-cup/tournament.py
@@ -1,32 +1,47 @@
-# Simulate a sports tournament
-
+# -- Simulate a sports tournament
+from sys import argv, exit
 import csv
-import sys
 import random
 
-# Number of simluations to run
+
+# -- Number of simulations to run
 N = 1000
 
 
 def main():
-
-    # Ensure correct usage
-    if len(sys.argv) != 2:
-        sys.exit("Usage: python tournament.py FILENAME")
+    """This is the main function."""
+    
+    # -- Ensure correct usage:
+    if len(argv) != 2:
+        exit("Usage: python tournament.py FILENAME")
 
     teams = []
-    # TODO: Read teams into memory from file
+    
+    # -- Read teams into memory from file:
+    with open(argv[1]) as file:
+        reader = csv.DictReader(file)
+        for team in reader:
+            team["rating"] = int(team["rating"])
+            teams.append(team)
 
     counts = {}
-    # TODO: Simulate N tournaments and keep track of win counts
+    
+    # -- Simulate N tournaments and keep track of win counts:
+    for i in range(N):
+        winner = simulate_tournament(teams)
+        
+        if winner in counts:
+            counts[winner] += 1
+        else:
+            counts[winner] = 1
 
-    # Print each team's chances of winning, according to simulation
+    # -- Print each team's chances of winning, according to simulation:
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
 
 
 def simulate_game(team1, team2):
-    """Simulate a game. Return True if team1 wins, False otherwise."""
+    """ Simulate a game. Return True if team1 wins, False otherwise."""
     rating1 = team1["rating"]
     rating2 = team2["rating"]
     probability = 1 / (1 + 10 ** ((rating2 - rating1) / 600))
@@ -37,7 +52,7 @@ def simulate_round(teams):
     """Simulate a round. Return a list of winning teams."""
     winners = []
 
-    # Simulate games for all pairs of teams
+    # -- Simulate games for all pairs of teams:
     for i in range(0, len(teams), 2):
         if simulate_game(teams[i], teams[i + 1]):
             winners.append(teams[i])
@@ -49,8 +64,13 @@ def simulate_round(teams):
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    # TODO
+    
+    while len(teams) > 1:
+        teams = simulate_round(teams)
+    
+    return teams[0]["team"]
 
 
+# -- Run the program:
 if __name__ == "__main__":
     main()
