b238bfeebbb71a37d4f5d39d4fa5687f99367321
dev_neil_a <neilallwood@icloud.com>
2023-12-15 16:00:36 +0000

    Adjusted USD function location and used it for price

--- week-09-flask-ajax/finance/helpers.py
+++ week-09-flask-ajax/finance/helpers.py
@@ -39,6 +39,11 @@ def login_required(f):
     return decorated_function
 
 
+def usd(value):
+    """Format value as USD."""
+    return f"${value:,.2f}"
+
+
 def lookup(symbol):
     """Look up quote for symbol."""
 
@@ -59,20 +64,16 @@ def lookup(symbol):
     try:
         response = requests.get(url, cookies={"session": str(uuid.uuid4())}, headers={"User-Agent": "python-requests", "Accept": "*/*"})
         response.raise_for_status()
-
+        print(response.content)
         # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
         quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
         quotes.reverse()
         price = round(float(quotes[0]["Adj Close"]), 2)
         return {
             "name": symbol,
-            "price": price,
+            "price": usd(price),
             "symbol": symbol
         }
     except (requests.RequestException, ValueError, KeyError, IndexError):
         return None
 
-
-def usd(value):
-    """Format value as USD."""
-    return f"${value:,.2f}"
